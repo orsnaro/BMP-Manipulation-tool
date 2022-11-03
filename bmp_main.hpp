ad9d9763773dc9421da139d9a140521d261f7199
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-11-03 17:53:54 +0200

    #

---
+++ bmp_main.hpp
@@ -0,0 +1,6 @@
+#pragma once 
+using namespace std;
+#include <bits/stdc++.h>
+#include <filesystem>
+// fopen is for 'C' and most of c file i/o is defined in 'stdio.h'
+//  ifstream is for 'C++' and defined in 'filesystem' dont forget that 
