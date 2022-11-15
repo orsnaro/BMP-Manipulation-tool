090ee8e63cc36a27806003621ff87f1a1fed6b93
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-11-16 00:15:55 +0200

    #

--- testing/on_BINfilesystem.cpp
+++ testing/on_BINfilesystem.cpp
@@ -1,9 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int main (){
+    ofstream toFile;
+    ifstream fromFile;
+    string path = {};
+    cout << ">> Please enter you '.bmp' Image path ( dont forget '.bmp' !) :" << endl;
+    cin >> path ;
 
-cout << "omar" ;
+    
+    fromFile.open(path, ios::binary | ios::in);
+    while(fromFile.fail()){
+        cerr << "ERROR FINDING IMAGE!! " << endl <<  "please re-enter image path!:" << endl;
+        cin >> path;
+    } 
+    cout << "Image" << path << " is found!" <<endl 
+    <<"begin Importing data proccess ..."<< endl;
 
-    return 0; 
+    toFile.write()
+
+    if ( fromFile.is_open() ) fromFile.close();
+    if (toFile.is_open()) toFile.close();
+    return 0 ;
 }
