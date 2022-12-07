84c02faa42a3a1105db9edcaaf2b1d66b0dbffae
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-12-07 22:31:58 +0200

    #

---
+++ cv_cpp/build/CMakeFiles/cv_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv_cpp.dir/main.cpp.obj"
+  "CMakeFiles/cv_cpp.dir/main.cpp.obj.d"
+  "cv_cpp.exe"
+  "cv_cpp.exe.manifest"
+  "cv_cpp.pdb"
+  "libcv_cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cv_cpp/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/OmarPc/repo_CHROMA_BMP_proj/cv_cpp
+# Build directory: C:/Users/OmarPc/repo_CHROMA_BMP_proj/cv_cpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
