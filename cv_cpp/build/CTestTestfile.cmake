0962232ebb3e0ca2075f27d59828dab7934d99f8
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-12-14 00:18:21 +0200

    # add most sources that i'll need + reorganize the files in repo #

--- cv_cpp/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/OmarPc/repo_CHROMA_BMP_proj/cv_cpp
-# Build directory: C:/Users/OmarPc/repo_CHROMA_BMP_proj/cv_cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
