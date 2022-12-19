cb00a2324df7719091ff0b403d320bb0e475eef2
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-12-19 22:03:38 +0200

    # separate enums from init_bmp.hpp  to new header #

---
+++ app_enums.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+
+          // --------------- ENUMS SECTION ----------------
+typedef short int state_t;
+enum class Signals_BMPstruct : state_t {
+    //all valid states = 0 
+    OK_FILE_TYPE, 
+    VALID_OFFSET  = 0, 
+    VALID_DIB_SZ = 0, 
+    OK_IMAGES_SIZE = 0,
+    VALID_COMPRESSION = 0, 
+    VALID_BIT_DEPTH = 0 ,
+    VALID_PADDING = 0, 
+
+    //all invalid states != 0 and unique 
+    INVALID_FILE_TYPE,
+    INVALID_OFFSET,
+    INVALID_DIB_SZ,
+    BAD_IMAGES_SIZE,
+    INVALID_COMPRESSION,
+    INVALID_BIT_DEPTH,
+    INVALID_PADDING //if read wrong or paddings  not 0x00 valued
+};
+
+enum class Signals_app : state_t {
+    //all valid states = 0
+
+    // all invalid states != 0 and unique
+
+};
--- init_bmp.hpp
+++ init_bmp.hpp
@@ -1,5 +1,6 @@
 #pragma once 
 #include <bits/stdc++.h>
+#include "app_enums"
 //#include <filessytem> //in stdc++.h
 
 
@@ -12,34 +13,6 @@
 
 
 
-          // --------------- ENUMS SECTION ----------------
-typedef short int state_t;
-enum class Signals_BMPstruct : state_t {
-    //all valid states = 0 
-    OK_FILE_TYPE, 
-    VALID_OFFSET  = 0, 
-    VALID_DIB_SZ = 0, 
-    OK_IMAGES_SIZE = 0,
-    VALID_COMPRESSION = 0, 
-    VALID_BIT_DEPTH = 0 ,
-    VALID_PADDING = 0, 
-
-    //all invalid states != 0 and unique 
-    INVALID_FILE_TYPE,
-    INVALID_OFFSET,
-    INVALID_DIB_SZ,
-    BAD_IMAGES_SIZE,
-    INVALID_COMPRESSION,
-    INVALID_BIT_DEPTH,
-    INVALID_PADDING //if read wrong or paddings  not 0x00 valued
-};
-
-enum class Signals_app : state_t {
-    //all valid states = 0
-
-    // all invalid states != 0 and unique
-
-};
 
            //--------------  PARENT CLASSES SECTION ----------------
 
