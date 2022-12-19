6c9de5215bfc6b3e819d80626322d2a83891e2f8
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-12-19 03:11:18 +0200

    # i'll finish this enum class of signals maybe will use hex dump from terminal and system library in cpp to read the whoe image # writing will use file stream

--- init_bmp.hpp
+++ init_bmp.hpp
@@ -1,5 +1,17 @@
 #pragma once 
 #include <bits/stdc++.h>
+#include <sys
 
 //load image  , do checks funcs , resize , cvrt to gray scale
 //TODO : init linikng to gui here ( my 1st cpp gui ever <3 )
+
+typedef short int state_t;
+
+enum class Signals : state_t {
+    ACCEPTED_FILE_TYPE,INVALID_FILE_TYPE,
+    VALID_OFFSET , INVALID_OFFSET,
+    VALID_DIB_SZ ,INVALID_DIB_SZ,
+
+    IMAGES_SIZE_BAD , IMAGES_SIZE_OK
+    
+};
